679867fc3bb00b0106f2e7d60221b6c8ac9b8e82
Sean Cribbs <sean_cribbs@cable.comcast.com>
2018-03-29 14:32:13 -0500

    Bump to v1.1.1

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule Dialyzex.Mixfile do
     [
       app: :dialyzex,
       description: description(),
-      version: "1.1.0",
+      version: "1.1.1",
       elixir: "~> 1.5",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
